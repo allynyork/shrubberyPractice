3aac5ec549dc3b8d0f4ecf5570a1cb7b4dd3bffa
allynyork <allynyork@gmail.com>
2015-07-23 15:02:16 -0700

    style update

--- shrubbery.rb
+++ shrubbery.rb
@@ -9,13 +9,13 @@ rescue ArgumentError => e
   puts "Could not parse YAML: #{e.message}"
 end
 
-b = Watir::Browser.new #:chrome
-b.goto "https://www.google.com/maps/"
+browser = Watir::Browser.new #:chrome
+browser.goto "https://www.google.com/maps/"
 
 sleep 2 
 
 # does the search box exist?
-search_box_exist = b.text_field(:class => 'tactile-searchbox-input').exists?
+search_box_exist = browser.text_field(:class => 'tactile-searchbox-input').exists?
 if search_box_exist == true
   puts 'yay, we have a directions search box!'
 else 
@@ -23,25 +23,25 @@ else
 end
  
 # open the direction search box
-b.button(:id => 'searchbox-directions').click
+browser.button(:id => 'searchbox-directions').click
 
 # start point
-b.text_field(:placeholder => 'Choose starting point, or click on the map...').wait_until_present
-b.text_field(:placeholder => 'Choose starting point, or click on the map...').set laden.fetch('start')
+browser.text_field(:placeholder => 'Choose starting point, or click on the map...').wait_until_present
+browser.text_field(:placeholder => 'Choose starting point, or click on the map...').set laden.fetch('start')
 
 # i deserve a drink. let's go to the Whiskey Library 
-b.text_field(:placeholder => 'Choose destination, or click on the map...').set laden.fetch('end')
+browser.text_field(:placeholder => 'Choose destination, or click on the map...').set laden.fetch('end')
 
 # select travel mode and get directions
-b.div(:class => laden.fetch('mode')).click
+browser.div(:class => laden.fetch('mode')).click
 
 # expand directions
-b.a(:class => 'cards-directions-details-link').wait_until_present
-b.a(:class => 'cards-directions-details-link').click 
+browser.a(:class => 'cards-directions-details-link').wait_until_present
+browser.a(:class => 'cards-directions-details-link').click 
 
 # read directions
-directions = b.div(:class => 'descriptionless').wait_until_present
-directions = b.div(:class => 'descriptionless').text
+directions = browser.div(:class => 'descriptionless').wait_until_present
+directions = browser.div(:class => 'descriptionless').text
 puts directions # look at those pretty directions!
 
 # save to csv
@@ -52,4 +52,4 @@ end
 
 
 # obtain a screenshot of the map with the route for a "future requirement".
-b.screenshot.save 'shrubberyscreenshot.png'
+browser.screenshot.save 'shrubberyscreenshot.png'
